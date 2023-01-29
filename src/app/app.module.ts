7b7eca625655148e187bfa88da67d7d0541032d7
MRcraigchick <craigchick@outlook.com>
2023-01-29 09:33:48 +0100

    Added router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MaterialModule } from './shared/material/material.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './core/components/toolbar/toolbar.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, AppRoutingModule],
+  declarations: [AppComponent, ToolbarComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/material/material.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule, MatToolbar } from '@angular/material/toolbar';
+
+@NgModule({
+  imports: [MatToolbarModule],
+  exports: [MatToolbar],
+})
+export class MaterialModule {}
