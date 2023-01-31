85c028d904916fc254a8ba84dd2bb2571f3fc3a4
Craig <craigchick@outlook.com>
2023-01-31 09:58:24 +0100

    Re-structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MaterialModule } from './core/modules/material.module';
+import { MaterialModule } from './modules/material.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { LogoComponent } from './shared/components/logo/logo.component';
-import { ToolbarComponent } from './shared/components/toolbar/toolbar.component';
-import { ThemeSwitchComponent } from './shared/components/theme-switch/theme-switch.component';
-import { HomeComponent } from './core/pages/home/home.component';
-import { IndexComponent } from './core/pages/index/index.component';
-import { LoginComponent } from './core/pages/login/login.component';
-import { SignupComponent } from './core/pages/signup/signup.component';
-import { NewtaskComponent } from './core/pages/newtask/newtask.component';
-import { DashboardComponent } from './shared/layouts/dashboard/dashboard.component';
-import { AuthenticationComponent } from './shared/layouts/authentication/authentication.component';
-import { TaskCardComponent } from './shared/components/task-card/task-card.component';
-import { BinComponent } from './shared/icons/bin/bin.component';
-import { PencilComponent } from './shared/icons/pencil/pencil.component';
+import { LogoComponent } from './shared/ui/logo/logo.component';
+import { ToolbarComponent } from './shared/ui/toolbar/toolbar.component';
+import { ThemeSwitchComponent } from './shared/ui/theme-switch/theme-switch.component';
+import { HomeComponent } from './home/ui/home.component';
+import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NewtaskComponent } from './newtask/newtask.component';
+import { DashboardComponent } from './shared/ui/layouts/dashboard/dashboard.component';
+import { AuthenticationComponent } from './shared/ui/layouts/authentication/authentication.component';
+import { TaskCardComponent } from './shared/ui/task-card/task-card.component';
+import { BinComponent } from './shared/ui/icons/bin/bin.component';
+import { PencilComponent } from './shared/ui/icons/pencil/pencil.component';
 
 @NgModule({
   imports: [
